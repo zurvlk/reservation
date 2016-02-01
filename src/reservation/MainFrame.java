3fbc965184e11815c4ffd2a62b80233eae5eb3df
zhuravlik <2rs.kei@gmail.com>
2016-02-01 17:10:21 +0900

    SQL接続確認

--- src/reservation/MainFrame.java
+++ src/reservation/MainFrame.java
@@ -18,61 +18,57 @@ import java.awt.event.WindowListener;
 public class MainFrame extends Frame implements ActionListener,WindowListener,KeyListener{
 
 	ReservationControl reservationControl;
+	Panel panelNorth; 		//上部パネル
+	Panel panelNorthSub1; 	//上部パネルの上
+	Panel panelNorthSub2; 	//上部パネルの中央
+	Panel panelNorthSub3; 	//上部パネルの下
+	Panel panelMid;			//中央パネル
+	Panel panelSouth;		//下部パネル
+
+	Button buttonLog;		// ログイン ・ ログアウト ボタン
+	Button buttonExplanation;	// 施設概要 説明ボタン
+	Button buttonVacancy;	// 空き状況確認
+	Button buttonReservation;	// 新規予約ボタン
+	Button buttonConfirm;	// 予約の確認
+	Button buttonCancel;	// 予約のキャンセルボタン
+
+	ChoiceFacility choiceFacility;	// 施設選択用選択ボックス
+	TextField tfYear, tfMonth, tfDay;	//年月日のテキストフィールド
+	TextArea textMessage;	// 結果表示用メッセージ欄
+
 	
-	Panel panelNorth; //上部パネル
-	Panel panelNorthSub1;
-	Panel panelNorthSub2;
-	Panel panelNorthSub3;
-	
-	Panel panelMid;	//中央パネル
-	Panel panelSouth; //下部パネル
-	
-	Button buttonLog;
-	Button buttonExplanation;
-	Button buttonVacancy;
-	Button buttonReservation;
-	Button buttonConfirm;
-	Button buttonCancel;
-	
-	ChoiceFacility choiceFacility;
-	TextField tfYear,tfMonth,tfDay;
-	TextArea textMessage;
-	
-	
-	public MainFrame(ReservationControl reservationControl)
-throws HeadlessException{	
+	public MainFrame(ReservationControl reservationControl) throws HeadlessException {
 		this.reservationControl = reservationControl;
-		addWindowListener(this);
-		addKeyListener(this);
-		
-		//ボタン生成
+		// ボタンの生成
 		buttonLog = new Button("ログイン");
-		buttonExplanation = new Button("施設概要");
-		buttonVacancy = new Button("空き状況照会");
-		buttonReservation = new Button("新規予約");
-		buttonConfirm = new Button("予約確認");
-		buttonCancel = new Button("予約キャンセル");
+		buttonExplanation = new Button(" 施設概要");
+		buttonVacancy = new Button(" 空き状況確認");
+		buttonReservation = new Button(" 新規予約");
+		buttonConfirm = new Button(" 予約の確認");
+		buttonCancel = new Button(" 予約のキャンセル");
 		
-		//設備チョイスボックス生成
+		// 設備チョイスボックスの生成
 		choiceFacility = new ChoiceFacility();
 		tfYear = new TextField("",4);
 		tfMonth = new TextField("",2);
 		tfDay = new TextField("",2);
-		
-		//レイアウトマネージャーにBorderLayout設定
+
+		// 上中下のパネルを使うため,レイアウトマネージャーにBorderLayoutを設定
 		setLayout( new BorderLayout());
-		
-		//パネル上段
+
+		// 上部パネルの上パネルに 予約システム というラベルと [ログイン]ボタンを追加
 		panelNorthSub1 = new Panel();
-		panelNorthSub1.add(new Label("施設予約システム"));
+		panelNorthSub1.add(new Label("施設予約システム "));
 		panelNorthSub1.add(buttonLog);
-		
+
+		// 上部パネルの中央パネルに 施設[施設名選択]チョイス[概要説明]ボタンを追加
 		panelNorthSub2 = new Panel();
 		panelNorthSub2.add(new Label("施設 "));
-		panelNorthSub2.add(choiceFacility);
+		panelNorthSub2.add( choiceFacility);
 		panelNorthSub2.add(new Label(" "));
-		panelNorthSub2.add(buttonExplanation);
-		
+		panelNorthSub2.add( buttonExplanation);
+
+		// 上部パネルの下パネルに年月日入力欄と 空き状況確認ボタンを追加
 		panelNorthSub3 = new Panel();
 		panelNorthSub3.add(new Label(" "));
 		panelNorthSub3.add(tfYear);
@@ -82,97 +78,113 @@ throws HeadlessException{
 		panelNorthSub3.add(tfDay);
 		panelNorthSub3.add(new Label("日 "));
 		panelNorthSub3.add( buttonVacancy);
-		
-		
-		textMessage = new TextArea( 20, 80);
-		textMessage.setEditable(false);
-		panelNorthSub3.add(textMessage);
-		
+
+		// 上部パネルに3つのパネルを追加
 		panelNorth = new Panel(new BorderLayout());
 		panelNorth.add(panelNorthSub1, BorderLayout.NORTH);
 		panelNorth.add(panelNorthSub2, BorderLayout.CENTER);
 		panelNorth.add(panelNorthSub3, BorderLayout.SOUTH);
 
+		// メイン画面(MainFrame)に上部パネルを追加
 		add(panelNorth,BorderLayout.NORTH);
-			
+
+		// 中央パネルにテキストメッセージ欄を設定
 		panelMid = new Panel();
-		
-		//パネル下段
+		textMessage = new TextArea( 20, 80);
+		textMessage.setEditable(false);
+		panelMid.add(textMessage);
+
+		// メイン画面(MainFrame)に中央パネルを追加
+		add( panelMid,BorderLayout.CENTER);
+
+		// 下部パネルにボタンを設定
 		panelSouth = new Panel();
 		panelSouth.add(buttonReservation);
+		panelSouth.add(new Label(" "));
 		panelSouth.add(buttonConfirm);
+		panelSouth.add(new Label(" "));
 		panelSouth.add(buttonCancel);
-		
-		add(panelSouth, BorderLayout.CENTER);
-		
-		
-		
+
+		// メイン画面(MainFrame)に下部パネルを追加
+		add( panelSouth,BorderLayout.SOUTH);
+
+		//ボタンのアクションリスナの追加
+		buttonLog.addActionListener(this);
+		buttonExplanation.addActionListener(this);
+		buttonVacancy.addActionListener(this);
+		buttonReservation.addActionListener(this);
+		buttonConfirm.addActionListener(this);
+		buttonCancel.addActionListener(this);
+
+
+		addWindowListener(this);
+		addKeyListener(this);
 	}
-	
+
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void windowOpened(WindowEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void windowClosing(WindowEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		System.exit(0);
 		
 	}
 
 	@Override
 	public void windowClosed(WindowEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void windowIconified(WindowEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void windowActivated(WindowEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO Auto-generated method stub
 		String result = new String();
 		textMessage.setText("");
 		if ( e.getSource() == buttonVacancy){ // 空き状況確認ボタン
@@ -180,10 +192,7 @@ throws HeadlessException{
 		}
 		textMessage.setText(result);
 
-
-
-		
 	}
 
-	
 }
+
